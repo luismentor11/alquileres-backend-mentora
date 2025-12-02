b4c19852bf20520f01e09e5fbc5da5c46dde5413
luismentor11 <infomentora11@gmail.com>
2025-12-02 00:41:33 -0300

    Add Pydantic models for Propietario schema

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,1 +1,29 @@
+from pydantic import BaseModel
+from typing import Optional
+
+# -----------------------
+#   PROPIETARIOS
+# -----------------------
+
+class PropietarioBase(BaseModel):
+    nombre: str
+    telefono: Optional[str] = None
+    email: Optional[str] = None
+
+
+class PropietarioCreate(PropietarioBase):
+    pass
+
+
+class PropietarioUpdate(BaseModel):
+    nombre: Optional[str] = None
+    telefono: Optional[str] = None
+    email: Optional[str] = None
+
+
+class PropietarioOut(PropietarioBase):
+    id: int
+
+    class Config:
+        orm_mode = True
 
